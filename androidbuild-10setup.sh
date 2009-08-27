8c7712d32abe6b0ecfbef8b9fdd1c60ee34d1d40
Andy Isaacson <adi@hexapodia.org>
2009-08-27 13:23:51 -0700

    install s3cmd, s/Android/Noisedroid, and make /mnt/build
    Signed-off-by: Andy Isaacson <adi@hexapodia.org>

--- androidbuild-10setup.sh
+++ androidbuild-10setup.sh
@@ -18,13 +18,19 @@ apt-get -y install git-core gnupg flex bison gperf build-essential zip \
 
 update-java-alternatives -s java-1.5.0-sun || true
 
+apt-get -y install s3cmd
+
 mkdir -p /opt/repo/bin
 curl http://android.git.kernel.org/repo > /opt/repo/bin/repo
 chmod +x /opt/repo/bin/repo
 md5sum /opt/repo/bin/repo
 
-adduser --disabled-password --gecos 'Android Buildbot' build
+adduser --disabled-password --gecos 'Noisedroid Buildbot' build
+
 mkdir -p ~build/.ssh
 cp /root/.ssh/authorized_keys ~build/.ssh
 chown -R build ~build/.ssh
 chmod -R u+rX,go-rwx ~build/.ssh
+
+mkdir /mnt/build
+chown build /mnt/build
