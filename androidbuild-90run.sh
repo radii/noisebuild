fa2e56ac1531b8fefc3dfb4f76d16fa53689158e
Andy Isaacson <adi@hexapodia.org>
2009-09-12 12:34:09 -0700

    build with -j2 and fix deliverable paths
    Build with -j2 since we're on a muticpu instance.
    Fix deliverable paths to conform to reality.
    
    Signed-off-by: Andy Isaacson <adi@hexapodia.org>

--- androidbuild-90run.sh
+++ androidbuild-90run.sh
@@ -8,14 +8,14 @@ cd /mnt/build/mydroid
 
 . build/envsetup.sh
 lunch htc_dream-eng
-make
+make -j2
 
 log "make exited with code $?"
 
-tar czf $buildnum.tar.gz mydroid/out/target/product/dream/[^o]*
+tar czf $buildnum.tar.gz out/target/product/dream-open/[^o]*
 s3cmd put $buildnum.tar.gz s3://$BUCKET/
-s3cmd put out/target/product/dream/system.img s3://$BUCKET/$buildnum-system.img
-s3cmd put out/target/product/dream/ramdisk.img s3://$BUCKET/$buildnum-ramdisk.img
-s3cmd put out/target/product/dream/userdata.img s3://$BUCKET/$buildnum-$userdata.img
+s3cmd put out/target/product/dream-open/system.img s3://$BUCKET/$buildnum-system.img
+s3cmd put out/target/product/dream-open/ramdisk.img s3://$BUCKET/$buildnum-ramdisk.img
+s3cmd put out/target/product/dream-open/userdata.img s3://$BUCKET/$buildnum-$userdata.img
 
 log "deliverables published for $buildnum"
