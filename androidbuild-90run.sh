5d3c3541e9a708bccd268d8d866824763fb231ac
Andy Isaacson <adi@hexapodia.org>
2009-09-13 17:17:20 -0700

    rework how build tarball is constructed and improve s3 reporting
    Build the tarball from out/target/product/ and use --exclude rather
    than janky glob.
    
    And, print the results of the s3 operations.
    
    Signed-off-by: Andy Isaacson <adi@hexapodia.org>

--- androidbuild-90run.sh
+++ androidbuild-90run.sh
@@ -12,10 +12,12 @@ make -j2
 
 log "make exited with code $?"
 
-tar czf $buildnum.tar.gz out/target/product/dream-open/[^o]*
-s3cmd put -P $buildnum.tar.gz s3://$BUCKET/$buildnum/
+tar -czf $buildnum.tar.gz -C out/target/product --exclude=dream-open/obj dream-open
+s3cmd put -P $buildnum.tar.gz s3://$BUCKET/$buildnum/$buildnum.tar.gz
 s3cmd put -P out/target/product/dream-open/system.img s3://$BUCKET/$buildnum/system.img
 s3cmd put -P out/target/product/dream-open/ramdisk.img s3://$BUCKET/$buildnum/ramdisk.img
 s3cmd put -P out/target/product/dream-open/userdata.img s3://$BUCKET/$buildnum/userdata.img
+
+s3cmd ls s3://$BUCKET/$buildnum/
 
 log "deliverables published for $buildnum"
